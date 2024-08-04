b18553895d13a619005f9a7f0af570549758875d
arunprashar <arunkumarom.613@gmail.com>
2024-08-05 03:08:00 +0530

    socket url

--- public/src/context/SocketContext.jsx
+++ public/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-k4kz.onrender.com", {
+			const socket = io("https://chat-app-ngco.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
